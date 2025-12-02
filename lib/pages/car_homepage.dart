934cbbc89183d9b81614e79bb999dc1399f304fc
ghatraxion <agh.arrafif@gmail.com>
2025-12-02 14:15:25 +0700

    fixing the reponsive

--- lib/pages/car_homepage.dart
+++ lib/pages/car_homepage.dart
@@ -5,17 +5,37 @@ import 'package:flutter_application_1/data/car_data.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  int getResponsiveColumn(double width) {
+    if (width >= 1600) return 6;
+    if (width >= 1200) return 5;
+    if (width >= 800) return 4;
+    if (width >= 480) return 3;
+    return 2;
+  }
+
+  double getAspectRatio(double width) {
+    if (width >= 1600) return 1;
+    if (width >= 1200) return 0.9;
+    if (width >= 800) return 0.8;
+    if (width >= 480) return 0.75;
+    return 0.65;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final columnCount = getResponsiveColumn(width);
+    final aspectRatio = getAspectRatio(width);
+
     return Scaffold(
       appBar: AppBar(title: const Text("Future Cars Catalog")),
       body: GridView.builder(
         padding: const EdgeInsets.all(12),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 0.8,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: columnCount,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          childAspectRatio: aspectRatio,
         ),
         itemCount: carList.length,
         itemBuilder: (context, index) {
@@ -28,6 +48,11 @@ class HomePage extends StatelessWidget {
               );
             },
             child: Card(
+              elevation: 4,
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
               child: Column(
                 children: [
                   Expanded(
@@ -39,9 +64,10 @@ class HomePage extends StatelessWidget {
                     child: Center(
                       child: Text(
                         car.name,
+                        textAlign: TextAlign.center,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
-                          fontSize: 16,
+                          fontSize: 14,
                         ),
                       ),
                     ),
