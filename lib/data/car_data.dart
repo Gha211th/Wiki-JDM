73cd867860cc1dc835df34aeffdf1576ac7289b4
ghatraxion <agh.arrafif@gmail.com>
2025-12-02 13:47:01 +0700

    First commit

---
+++ lib/data/car_data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_application_1/models/car.dart';
+
+List<Car> carList = [
+  Car(
+    name: 'Honda NSX',
+    image: "assets/images/nsx.jpg",
+    description: "The ferrari of japan~",
+    color: 'red',
+    attributes: {
+      "Top Speed": "265 km/h",
+      "engine": "nothing",
+      "weight": "970kg",
+    },
+    price: 900,
+  ),
+];
---
+++ lib/models/car.dart
@@ -0,0 +1,17 @@
+class Car {
+  final String name;
+  final String image;
+  final String description;
+  final String color;
+  final Map<String, String> attributes;
+  final double price;
+
+  Car({
+    required this.name,
+    required this.image,
+    required this.description,
+    required this.color,
+    required this.attributes,
+    required this.price,
+  });
+}
